4c30971d7a3c08dcbe26b0ee11a777cc705f50b3
jinhuiqian <1286122850@qq.com>
2020-12-03 20:20:36 +0800

    first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,12 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <div class="row">
+      <div v-for="(card,index) in cards" :key="index" class="cards">
+        <h3>{{ card.title }}</h3>
+        <img :src="card.bgImg" class="cover"/>
+        <p>{{card.content}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,25 +14,52 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    msg: String,
+  },
+  data(){
+    return{
+      cards: [],  
+    }
+  },
+  created(){
+    this.axios.get('http://localhost:8080/api/cards').then((res) => {
+      console.log(res.data.data)
+      this.cards = res.data.data
+      console.log(JSON.stringify(this.cards));
+    })
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+@font-face {
+  font-family: 'hot-nijia';
+  src: url('../fonts/HOT-Ninja Std R.otf');
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.container {
+  width: 80%;
+  margin: 0 auto;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.row {
+  display: flex;
+  flex-wrap: wrap;
 }
-a {
-  color: #42b983;
+.cards {
+  font-family: 'hot-nijia';
+  font-size: 18px;
+  flex: 0 0 25%;
+  height: 300px;
+  background-color: #CCCCCC;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
 }
+.cover {
+  width: 100%;
+  height: 150px;
+  border-radius: 5px;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios,axios)
 
 Vue.config.productionTip = false
 
